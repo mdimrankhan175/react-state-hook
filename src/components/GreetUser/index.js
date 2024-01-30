771c3821a891a8ec63138f55360e029fdec639d8
imran <mdimrankhan175@gmail.com>
2024-01-30 18:57:42 +0000

    useState

---
+++ src/components/GreetUser/index.js
@@ -0,0 +1,22 @@
+import {useState} from 'react'
+import {
+  MainContainer,
+  NameInput,
+  MsgContent,
+  NameText,
+} from './styledComponents'
+
+const GreetUser = () => {
+  const [name, setName] = useState('Imran')
+  const onChangeName = event => setName(event.target.value)
+  return (
+    <MainContainer>
+      <NameInput type="text" placeholder="Your name" onChange={onChangeName} />
+      <MsgContent>
+        Hello <NameText>{name}</NameText>
+      </MsgContent>
+    </MainContainer>
+  )
+}
+
+export default GreetUser
